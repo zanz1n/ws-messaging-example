e0ffd51cfb7985fd42e7de8a2ce7d5581695e86e
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-03-02 15:46:05 -0300

    add custom background to app

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 export default function App() {
     return (
         <div className="App">
+            <div className="App-gb-img"></div>
             <Outlet/>
         </div>
     );
