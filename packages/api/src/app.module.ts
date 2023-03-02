bb2db4e61fca8a2ef0b3b4fb8163b8f0551fdecf
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-03-02 17:15:13 -0300

    add auth module

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "./prisma/prisma.module.js";
+import { AuthModule } from "./auth/auth.module.js";
 
 @Module({
-    imports: [PrismaModule]
+    imports: [PrismaModule, AuthModule]
 })
 export class AppModule {}
