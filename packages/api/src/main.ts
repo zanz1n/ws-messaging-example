6373eff05f97dcfed55957678b5914bdd53739aa
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-03-03 14:13:22 -0300

    add validator pipe in nest app

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
 import { AppModule } from "./app.module.js";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+        rawBody: true,
+    });
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            whitelist: true,
+            forbidNonWhitelisted: true
+        })
+    );
+
     await app.listen(3333);
 }
 bootstrap();
