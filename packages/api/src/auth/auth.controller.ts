c0314e48143ba2653bdf463285ddbf98c5929e33
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-03-03 14:29:20 -0300

    add register and @me auth routes

--- packages/api/src/auth/auth.controller.ts
+++ packages/api/src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Post, Req, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Post, Req, UseGuards } from "@nestjs/common";
 import { Request } from "express";
 import { AuthService } from "./auth.service.js";
+import { JwtAuthGuard } from "./strategies/guards/jwt-auth.guard.js";
 import { LocalAuthGuard } from "./strategies/guards/local-auth.guard.js";
+import { RegisterUserDto } from "./types/register-user-payload.dto.js";
 
 @Controller("/auth")
 export class AuthController {
@@ -11,5 +13,16 @@ export class AuthController {
     @UseGuards(LocalAuthGuard)
     async login(@Req() req: Request) {
         return this.authService.login(req.user as any);
+    }
+
+    @Post("/register")
+    async register(@Body() body: RegisterUserDto) {
+        return this.authService.register(body.username, body.password);
+    }
+
+    @Get("/@me")
+    @UseGuards(JwtAuthGuard)
+    getProfile(@Req() req: Request) {
+        return req.user;
     }
 }
