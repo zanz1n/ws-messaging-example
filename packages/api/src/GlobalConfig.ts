143bbc570f491af3633a68ba695cf3fe7005d466
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-03-03 15:18:37 -0300

    add dev mode check in global config

--- packages/api/src/GlobalConfig.ts
+++ packages/api/src/GlobalConfig.ts
@@ -2,4 +2,5 @@ export const GlobalConfig = {
     DbUrl: process.env.DATABASE_URL,
     JwtSecret: process.env.JWT_SECRET,
     BCryptSalt: Number(process.env.BCRYPT_SALT),
+    DevMode: process.env.NODE_ENV === "development",
 };
