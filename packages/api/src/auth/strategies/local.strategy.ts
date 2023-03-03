54eac675a10401803d6bc923339657ff7a856206
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-03-03 14:23:56 -0300

    auth strategy must be injectable

--- packages/api/src/auth/strategies/local.strategy.ts
+++ packages/api/src/auth/strategies/local.strategy.ts
@@ -1,9 +1,10 @@
-import { UnauthorizedException } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { User } from "@prisma/client";
 import { Strategy } from "passport-local";
 import { AuthService } from "../auth.service.js";
 
+@Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
     constructor(private readonly authService: AuthService) {
         super();
