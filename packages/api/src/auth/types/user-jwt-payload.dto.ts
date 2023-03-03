5eb6a20567656e631d4d35d045f67cb621f153fb
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-03-03 16:33:38 -0300

    add jwt request typing helper

--- packages/api/src/auth/types/user-jwt-payload.dto.ts
+++ packages/api/src/auth/types/user-jwt-payload.dto.ts
@@ -1,4 +1,14 @@
+import { User } from "@prisma/client";
+
 export abstract class UserJwtPayloadDto {
     id: string;
     username: string;
+}
+
+export type JwtGuardedRequest<T extends Request> = T & {
+    user: UserJwtPayloadDto;
+}
+
+export type LocalGuardedRequest<T extends Request> = T & {
+    user: User;
 }
