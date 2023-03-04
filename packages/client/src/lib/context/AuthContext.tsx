db5da725f12a50d7a1f8fcb5218abfb8e2fadee5
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-03-04 00:43:48 -0300

    update auth context

--- packages/client/src/lib/context/AuthContext.tsx
+++ packages/client/src/lib/context/AuthContext.tsx
@@ -1,15 +1,23 @@
 import { createContext, useContext, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
+import { ClientConfig } from "../../ClientConfig";
 
 export interface AuthContext {
     token: string | null;
     login: (props: LoginProps) => Promise<boolean>;
     logout: () => Promise<void>;
+    register: (props: RegisterProps) => Promise<boolean>;
 }
 
 export interface LoginProps {
     username: string;
     password: string;
+}
+
+export interface RegisterProps {
+    username: string;
+    password: string;
+    confirmPassword: string;
 }
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -21,8 +29,32 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 
     const navigate = useNavigate();
 
+    async function register({ username, password, confirmPassword }: RegisterProps) {
+        return fetch(ClientConfig.ApiUri + "/auth/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ username, password, confirmPassword })
+        }).then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return false;
+        }).then((data: unknown) => {
+            if (data && typeof data == "object" && "access_token" in data && typeof data.access_token === "string") {
+                localStorage.setItem("token", data.access_token);
+                return true;
+            }
+            return false;
+        }).catch((err) => {
+            console.error(err);
+            return false;
+        });
+    }
+
     async function login({ username, password }: LoginProps): Promise<boolean> {
-        return fetch("/api/login", {
+        return fetch(ClientConfig.ApiUri + "/auth/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -34,8 +66,8 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
             }
             return false;
         }).then((data: unknown) => {
-            if (data && typeof data == "object" && "token" in data && typeof data.token === "string") {
-                localStorage.setItem("token", data.token);
+            if (data && typeof data == "object" && "access_token" in data && typeof data.access_token === "string") {
+                localStorage.setItem("token", data.access_token);
                 return true;
             }
             return false;
@@ -53,7 +85,8 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     const value = useMemo(() => ({
         token,
         login,
-        logout
+        logout,
+        register
     }), [token]);    
 
     return (
