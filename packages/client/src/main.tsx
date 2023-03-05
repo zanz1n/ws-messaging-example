af2d35e636e8aa00160358b55500256f3b401396
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-03-04 23:52:59 -0300

    remove strict mode rendering type

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,11 +1,8 @@
-import { StrictMode } from "react";
 import { RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { router } from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <StrictMode>
-        <RouterProvider router={router} />
-    </StrictMode>,
+    <RouterProvider router={router} />
 );
